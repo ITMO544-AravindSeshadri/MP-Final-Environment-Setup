8120e1b648d592780c65eae80d76fe87e3b78500
Aravind Seshadri <aravsesh1@gmail.com>
2015-11-21 12:38:08 -0600

    Path change

--- install-webserver.sh
+++ install-webserver.sh
@@ -3,9 +3,9 @@ sudo apt-get update && sudo apt-get install -y apache2 git php5 php5-mysql php5-
 touch /tmp/hello.txt
 echo "Hello" > /tmp/hello.txt
 cd /var/www/html
-sudo git clone https://github.com/ITMO544-AravindSeshadri/MP2-Application-Setup.git
-sudo mv -v MP2-Application-Setup/* /var/www/html
-sudo rm -rf /var/www/html/MP2-Application-Setup
+sudo git clone https://github.com/ITMO544-AravindSeshadri/MP-Final-Application-Setup.git
+sudo mv -v MP-Final-Application-Setup/* /var/www/html
+sudo rm -rf /var/www/html/MP-Final-Application-Setup
 cd /home/controller
 sudo curl -sS https://getcomposer.org/installer | php
 sudo php composer.phar require aws/aws-sdk-php
